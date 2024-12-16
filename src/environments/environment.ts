5d4802b4c3eafd82dd1d841c22fb3e2eb92179e3
VICTOR HUGO SOLIS HERRERA <1127755@horusur.corp>
2024-12-16 11:14:49 -0600

    url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://miapivictor.somee.com/api',
+  apiUrl: 'https://miapivictor.somee.com/api',
   _token: 'd27407260e4b49c3b50255a7a66987d4',
   _tokenR: '7fd1aaa8ca3f469da957b50c1feb8604',
   _tokenStorage: '81351cbec0e94954b0d6f8d614e5159d',
